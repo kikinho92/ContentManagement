9e7a1d082aeee18c0a98b33f4e2c9b51e861a32a
Kike Martín <jemartinp@gmail.com>
2023-05-04 10:07:04 +0200

    test

--- Library.Common/CommonConstants.cs
+++ Library.Common/CommonConstants.cs
@@ -8,9 +8,9 @@ namespace Library.Common
         /// Service base URL from localhost.
         /// Used by SDK constructor from services accesing other services internally for development enviroment.
         /// </summary>
-        public static readonly string SERVICE_LOCAL_URL_AUTH = "http://localhost:8001";
-        public static readonly string SERVICE_LOCAL_URL_USER = "http://localhost:8002";
-        public static readonly string SERVICE_LOCAL_URL_CONTENT = "http://localhost:8003";
-        public static readonly string SERVICE_LOCAL_URL_INTEGRATION = "http://localhost:8004";
+        public static readonly string SERVICE_LOCAL_URL_AUTH = "http://10.96.249.12:8001";
+        public static readonly string SERVICE_LOCAL_URL_USER = "http://10.96.249.12:8002";
+        public static readonly string SERVICE_LOCAL_URL_CONTENT = "http://10.96.249.12:8003";
+        public static readonly string SERVICE_LOCAL_URL_INTEGRATION = "http://10.96.249.12:8004";
     }
 }

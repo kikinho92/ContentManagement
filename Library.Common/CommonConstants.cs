1ee7a54005c95ed4252ead4df08f1ab0e3dfe126
Kike Martín <jemartinp@gmail.com>
2023-06-14 14:39:18 +0200

    Docker & Kubernetes files

--- Library.Common/CommonConstants.cs
+++ Library.Common/CommonConstants.cs
@@ -12,5 +12,10 @@ namespace Library.Common
         public static readonly string SERVICE_LOCAL_URL_USER = "http://10.96.249.12:8002";
         public static readonly string SERVICE_LOCAL_URL_CONTENT = "http://10.96.249.12:8003";
         public static readonly string SERVICE_LOCAL_URL_INTEGRATION = "http://10.96.249.12:8004";
+
+        /* public static readonly string SERVICE_LOCAL_URL_AUTH = "http://localhost:8001";
+        public static readonly string SERVICE_LOCAL_URL_USER = "http://localhost:8002";
+        public static readonly string SERVICE_LOCAL_URL_CONTENT = "http://localhost:8003";
+        public static readonly string SERVICE_LOCAL_URL_INTEGRATION = "http://localhost:8004"; */
     }
 }
